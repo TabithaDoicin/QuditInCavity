6f8b17558bc062cf2e6c3471ed1be8029da71cad
Tib Doicin <atdoicin@gmail.com>
2022-11-06 19:51:10 +0000

    Update multilevel_example.py

--- multilevel_example.py
+++ multilevel_example.py
@@ -63,23 +63,3 @@ ax.plot(wlist, np.log10(spec))
 # plt.title(r'$S_a(\omega)$ vs detuning for parameters: N=' + str(N) + r', D=' + str(D) + r', $g_{eff}$=' + str(geff)\
 #           + r', $\epsilon$=' + str(ep)+ r', $\omega_a$=' + str(wa) + r', $\omega_c$=' + str(wc) +',\n' r'$\kappa$=' + str(kappa)\
 #           + r',  $ \gamma$=' + str(gamma)+ r', $\gamma_d$=' + str(round(gamma_d,2)) + r', $\Lambda$=' + str(LAMBDA) + r', $\Omega$=' + str(omega) + r', $\zeta$='+str(zeta) + r', $\alpha$='+str(alpha), fontsize='small')
-
-
-# g2list = system.g2listcalc(system.a)
-# fig,ax=plt.subplots()
-# ax.plot(system.wl_list,g2list)
-# ax.set_yscale('log')
-# ax.set_ylabel(r'$g^{(2)}(0)$')
-# ax.set_xlabel(r'$(\omega_L-\omega_0)/g_{eff}$')
-# plt.title(r'$g^{(2)}(0)$ vs detuning for parameters: N=' + str(N) + r', D=' + str(D) + r', $g_{eff}$=' + str(geff)\
-#           + r', $\epsilon$=' + str(ep)+ r', $\omega_a$=' + str(wa) + r', $\omega_c$=' + str(wc) +',\n' r'$\kappa$=' + str(kappa)\
-#           + r',  $ \gamma$=' + str(gamma)+ r', $\gamma_d$=' + str(round(gamma_d,2)) + r', $\Lambda$=' + str(LAMBDA) + r', $\Omega$=' + str(omega) + r', $\zeta$='+str(zeta) + r', $\alpha$='+str(alpha), fontsize='small')
-
-# # system.ss_dm(driving=True)
-# darkstates = system.darkstate_proportion(driving=True)
-
-# fig2,ax2 = plt.subplots()
-# ax2.plot(system.wl_list,darkstates)
-# ax2.set_ylabel(r'$p_{dark}$')
-# ax2.set_xlabel(r'$(\omega_L-\omega_0)/g_{eff}$')
-# plt.title(r'corresponding dark state proportions...', fontsize='small')
