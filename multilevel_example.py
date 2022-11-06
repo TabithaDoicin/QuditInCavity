1b17c019cef4f3129861e0e3cea00762556221f0
Tib Doicin <atdoicin@gmail.com>
2022-11-06 19:47:18 +0000

    new stuff

--- multilevel_example.py
+++ multilevel_example.py
@@ -11,47 +11,59 @@ qutip.settings.has_mkl = False
 import matplotlib.pyplot as plt
 import simulation as t
 
-N = 4         # number of cavity fock states
-D = 2          #number of atomic states?
+
+N = 3         # number of cavity fock states
+D = 7          #number of atomic states?
 geff = 1
-ep=0.05*geff*2
+ep=0.05*geff
 wa = 0 # cavity and atom frequency
 wc = 0
-kappa = 0.05*geff        # cavity dissipation rate
+kappa = 1*geff        # cavity dissipation rate
 gamma = 0        # atom dissipation rate
 gamma_d = 0*kappa
-LAMBDA =0*kappa
+LAMBDA =0.01*kappa
 
-omega = 0.02*geff
-alpha= 0
-zeta=0
-
-zeta = -2j*omega/kappa*geff
 omega=0
-alpha=0#+2j*omega/kappa
-
+zeta=0
+alpha=0
 
 system = t.MultiLevel(N, D, geff, ep, wc, wa, kappa, gamma, gamma_d, LAMBDA, omega, zeta, alpha)
-Hlist = system.hamiltonian(300)
+H = system.hamiltonian()
 c_ops = system.collapse()
 
-#wlist = np.linspace(-1*np.pi *system.geff + system.wc, 1*np.pi *system.geff + system.wc, 600)
-#spec = spectrum(H, wlist, c_ops, system.adag, system.a)
+wlist = np.linspace(-1*np.pi *system.geff + system.wc, 1*np.pi *system.geff + system.wc, 5000)
+spec = spectrum(H, wlist, c_ops, system.adag, system.a)
 
-#fig, ax = plt.subplots()
-#ax.plot(wlist, np.log10(spec))
+fig, ax = plt.subplots()
+ax.plot(wlist, np.log10(spec))
 
-g2list = system.g2listcalc()
-fig,ax=plt.subplots()
-ax.plot(system.wl_list,g2list)
-ax.set_yscale('log')
-ax.set_ylabel(r'$g^{(2)}(0)$')
-ax.set_xlabel(r'$(\omega_L-\omega_0)/g_{eff}$')
-plt.title(r'$g^{(2)}(0)$ vs detuning for parameters: N=' + str(N) + r', D=' + str(D) + r', $g_{eff}$=' + str(geff)\
-          + r', $\epsilon$=' + str(ep)+ r', $\omega_a$=' + str(wa) + r', $\omega_c$=' + str(wc) +',\n' r'$\kappa$=' + str(kappa)\
-          + r',  $ \gamma$=' + str(gamma)+ r', $\gamma_d$=' + str(round(gamma_d,2)) + r', $\Lambda$=' + str(LAMBDA) + r', $\Omega$=' + str(omega) + r', $\zeta$='+str(zeta), fontsize='small')
+# g2list = system.g2listcalc()
+# fig,ax=plt.subplots()
+# ax.plot(system.wl_list,g2list)
+# ax.set_yscale('log')
+# ax.set_ylabel(r'$g^{(2)}(0)$')
+# ax.set_xlabel(r'$(\omega_L-\omega_0)/g_{eff}$')
+# plt.title(r'$g^{(2)}(0)$ vs detuning for parameters: N=' + str(N) + r', D=' + str(D) + r', $g_{eff}$=' + str(geff)\
+#           + r', $\epsilon$=' + str(ep)+ r', $\omega_a$=' + str(wa) + r', $\omega_c$=' + str(wc) +',\n' r'$\kappa$=' + str(kappa)\
+#           + r',  $ \gamma$=' + str(gamma)+ r', $\gamma_d$=' + str(round(gamma_d,2)) + r', $\Lambda$=' + str(LAMBDA) + r', $\Omega$=' + str(omega) + r', $\zeta$='+str(zeta), fontsize='small')
 
-# system.ss_dm(driving=True)
+# system = t.MultiLevel(N, D, geff, ep, wc, wa, kappa, gamma, gamma_d, LAMBDA, omega, zeta, alpha)
+# H = system.hamiltonian(100)
+# c_ops = system.collapse()
+
+# wlist = np.linspace(-1*np.pi *system.geff + system.wc, 1*np.pi *system.geff + system.wc, 200)
+# spec = spectrum(H, wlist, c_ops, system.adagori, system.aori)
+
+# fig, ax = plt.subplots()
+# ax.plot(wlist, spec)
+# ax.set_yscale('log')
+# ax.set_ylabel(r'$S_a(\omega)$')
+# ax.set_xlabel(r'$(\omega-\omega_0)/g_{eff}$')
+# plt.title(r'$S_a(\omega)$ vs detuning for parameters: N=' + str(N) + r', D=' + str(D) + r', $g_{eff}$=' + str(geff)\
+#           + r', $\epsilon$=' + str(ep)+ r', $\omega_a$=' + str(wa) + r', $\omega_c$=' + str(wc) +',\n' r'$\kappa$=' + str(kappa)\
+#           + r',  $ \gamma$=' + str(gamma)+ r', $\gamma_d$=' + str(round(gamma_d,2)) + r', $\Lambda$=' + str(LAMBDA) + r', $\Omega$=' + str(omega) + r', $\zeta$='+str(zeta) + r', $\alpha$='+str(alpha), fontsize='small')
+
+# # system.ss_dm(driving=True)
 # darkstates = system.darkstate_proportion(driving=True)
 
 # fig2,ax2 = plt.subplots()
