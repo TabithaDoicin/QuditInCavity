264f8d5a021b4842052e432b535e846aa42c7fd8
Tib Doicin <atdoicin@gmail.com>
2023-06-14 02:08:54 +0100

    added entropy

--- energyspectrum_usc.py
+++ energyspectrum_usc.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 import simulation as t
 
 N = 15             # number of cavity fock states #needs to be really high to properly classify eigenenergies
-D = 5             #number of atomic states
+D = 3             #number of atomic states
 geff = 1
 ep=0.01*geff
 wa = 1            # cavity and atom frequency
@@ -24,8 +24,8 @@ sys = t.MultiLevel(N, D, geff, ep, wc, wa, 0, 0, 0, 0, 0, 0, 0, rwa=True)
 
 #looking at geff variation
 geff_list_min = 0
-geff_list_max = 4
-geff_list_num = 300
+geff_list_max = 2
+geff_list_num = 100
 
 geff_list = np.linspace(geff_list_min, geff_list_max, geff_list_num)
 
@@ -34,9 +34,6 @@ systems_energies_rwa_list = np.empty([geff_list_num], dtype = object)
 
 systems_no_rwa_list = np.empty([geff_list_num], dtype = object)
 systems_energies_no_rwa_list = np.empty([geff_list_num], dtype = object)
-
-systems_MBS_list = np.empty([geff_list_num], dtype = object)
-systems_energies_MBS_list = np.empty([geff_list_num], dtype = object)
 
 systems_GMBS_list = np.empty([geff_list_num], dtype = object)
 systems_energies_GMBS_list = np.empty([geff_list_num], dtype = object)
@@ -53,11 +50,6 @@ for k in range(geff_list_num):
     systems_no_rwa_list[k].hamiltonian(suppress=True)
     systems_energies_no_rwa_list[k] = np.array(systems_no_rwa_list[k].H.eigenenergies())
     
-    systems_MBS_list[k] = t.DegenBlochSiegert(N, D, geff_list[k], wc, wa)
-    systems_MBS_list[k].hamiltonian()
-    systems_energies_MBS_list[k] = np.array(systems_MBS_list[k].H.eigenenergies())
-    
-    
     systems_GMBS_list[k] = t.GeneralBlochSiegert(N, D, geff_list[k], ep, wc, wa)
     systems_GMBS_list[k].hamiltonian()
     systems_energies_GMBS_list[k] = np.array(systems_GMBS_list[k].H.eigenenergies())
@@ -69,14 +61,12 @@ for k in range(geff_list_num):
         
 energy_rwa_list = np.empty([len(systems_energies_rwa_list[0])],dtype=object) #energy levels specifically!!
 energy_no_rwa_list = np.empty([len(systems_energies_no_rwa_list[0])],dtype=object) #energy levels specifically!!
-energy_MBS_list = np.empty([len(systems_energies_MBS_list[0])],dtype=object) #energy levels specifically!!
 energy_GMBS_list = np.empty([len(systems_energies_GMBS_list[0])],dtype=object) #energy levels specifically!!
 energy_GMBS_corrected_list = np.empty([len(systems_energies_GMBS_corrected_list[0])],dtype=object)
 
 for n in range(len(energy_rwa_list)): #the length is the same as N*D because hamiltonian diagonalisation is the amount of energy levels
     energy_rwa_list[n] = [systems_energies_rwa_list[k][n] for k in range(len(geff_list))]
     energy_no_rwa_list[n] =  [systems_energies_no_rwa_list[k][n] for k in range(len(geff_list))]
-    energy_MBS_list[n] =  [systems_energies_MBS_list[k][n] for k in range(len(geff_list))]
     energy_GMBS_list[n] =  [systems_energies_GMBS_list[k][n] for k in range(len(geff_list))]
     energy_GMBS_corrected_list[n] =  [systems_energies_GMBS_corrected_list[k][n] for k in range(len(geff_list))]
     
@@ -97,9 +87,8 @@ E0 = [(-g**2/(D-1)) * (D-2)/ep * (sp.special.digamma(k+D-1)-sp.special.digamma(k
 
 for n in range(len(energy_rwa_list)):#plotting
     MJC_line, = ax.plot(geff_list,energy_rwa_list[n], color = 'black', linestyle = 'dotted', label='MJC') #no rescaling?
-    #MQRM_line, = ax.plot(geff_list,energy_no_rwa_list[n]+1*additionscaling, color = 'red', label='MQRM')
-    #ax.plot(geff_list,energy_MBS_list[n]+1*additionscaling, color = 'blue',linestyle = 'dotted')
-    #GMBS_line, = ax.plot(geff_list,energy_GMBS_list[n]+1*additionscaling, color = 'blue',linestyle = 'dotted', label='GMBS')
+    MQRM_line, = ax.plot(geff_list,energy_no_rwa_list[n]+1*additionscaling, color = 'red', label='MQRM')
+    GMBS_line, = ax.plot(geff_list,energy_GMBS_list[n]+1*additionscaling, color = 'blue',linestyle = 'dotted', label='GMBS')
     #GMBS_gnd_analytical_line, = ax.plot(geff_list,E0+1*additionscaling, color = 'green', label = 'GND_GMBS')
     #GMBS_corrected_line, = ax.plot(geff_list,energy_GMBS_corrected_list[n]+1*additionscaling, color = 'fuchsia',linestyle = 'dotted', label='GMBS_corr')
 
---
+++ entropy_of_entanglement.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 12 00:44:22 2023
+
+@author: Tabitha
+"""
+
+import numpy as np
+from qutip import *
+import scipy as sp
+qutip.settings.has_mkl = False
+import matplotlib.pyplot as plt
+import simulation as t
+
+N = 10             # number of cavity fock states #needs to be really high to properly classify eigenenergies
+D = 10             #number of atomic states
+geff_forops = 1
+ep=0.2*geff_forops
+wa = 1            # cavity and atom frequency
+wc = 1
+
+#geff variation
+geff_list_min = 0
+geff_list_max = 1
+geff_list_num = 100
+geff_list = np.linspace(geff_list_min, geff_list_max, geff_list_num)
+
+systems_rwa_list = np.empty([geff_list_num], dtype = object)
+systems_gndstate_rwa_list = np.empty([geff_list_num], dtype = object)
+
+systems_no_rwa_list = np.empty([geff_list_num], dtype = object)
+systems_gndstate_no_rwa_list = np.empty([geff_list_num], dtype = object)
+
+systems_MBS_list = np.empty([geff_list_num], dtype = object)
+systems_gndstate_MBS_list = np.empty([geff_list_num], dtype = object)
+
+for k in range(geff_list_num):
+    systems_rwa_list[k] = t.MultiLevel(N, D, geff_list[k], ep, wc, wa, 0, 0, 0, 0, 0, 0, 0, rwa=True)
+    systems_rwa_list[k].hamiltonian(suppress=True)
+    systems_gndstate_rwa_list[k] = systems_rwa_list[k].H.groundstate()[1]
+    
+    systems_no_rwa_list[k] = t.MultiLevel(N, D, geff_list[k], ep, wc, wa, 0, 0, 0, 0, 0, 0, 0, rwa=False)
+    systems_no_rwa_list[k].hamiltonian(suppress=True)
+    systems_gndstate_no_rwa_list[k] = systems_no_rwa_list[k].H.groundstate()[1]
+    
+    systems_MBS_list[k] = t.GeneralBlochSiegert(N, D, geff_list[k], ep, wc, wa)
+    systems_MBS_list[k].hamiltonian()
+    systems_gndstate_MBS_list[k] = systems_MBS_list[k].Udag * systems_MBS_list[k].H.groundstate()[1] #basis changed even needed for entropy?
+
+entropy_cav_rwa = np.empty([geff_list_num], dtype= np.float128)
+entropy_ato_rwa = np.empty([geff_list_num], dtype= np.float128)
+
+entropy_cav_norwa = np.empty([geff_list_num], dtype= np.float128)
+entropy_ato_norwa = np.empty([geff_list_num], dtype= np.float128)
+
+entropy_cav_MBS = np.empty([geff_list_num], dtype= np.float128)
+entropy_ato_MBS = np.empty([geff_list_num], dtype= np.float128)
+
+for k in range(geff_list_num):
+    rho_cav_rwa = ptrace(systems_gndstate_rwa_list[k], 0)
+    rho_ato_rwa = ptrace(systems_gndstate_rwa_list[k], 1)
+    entropy_cav_rwa[k] = entropy_vn(rho_cav_rwa)
+    entropy_ato_rwa[k] = entropy_vn(rho_ato_rwa)
+    
+    rho_cav_norwa = ptrace(systems_gndstate_no_rwa_list[k], 0)
+    rho_ato_norwa = ptrace(systems_gndstate_no_rwa_list[k], 1)
+    entropy_cav_norwa[k] = entropy_vn(rho_cav_norwa)
+    entropy_ato_norwa[k] = entropy_vn(rho_ato_norwa)
+    
+    rho_cav_MBS = ptrace(systems_gndstate_MBS_list[k], 0)
+    rho_ato_MBS = ptrace(systems_gndstate_MBS_list[k], 1)
+    entropy_cav_MBS[k] = entropy_vn(rho_cav_MBS)
+    entropy_ato_MBS[k] = entropy_vn(rho_ato_MBS)
+    
+
+fig, ax = plt.subplots()
+ax.set_ylabel(r'$S$')
+ax.set_xlabel(r'$g_{eff}$')
+MJCcav, = ax.plot(geff_list, entropy_cav_rwa, label='MJC', color='black')
+MJCato, = ax.plot(geff_list, entropy_ato_rwa, label='MJC', color='black', linestyle = 'dotted')
+MQRMcav, = ax.plot(geff_list, entropy_cav_norwa, color='red', label='MQRM')
+MQRMato, = ax.plot(geff_list, entropy_ato_norwa, color='red', label='MQRM', linestyle = 'dotted')
+MBScav, = ax.plot(geff_list, entropy_cav_MBS, color='blue', label='MBS_full')
+MBSato, = ax.plot(geff_list, entropy_ato_MBS, color='blue', label='MBS_full', linestyle = 'dotted')
+
+plt.xlim(geff_list_min,geff_list_max)
+ax.legend(handles=[MJCcav, MJCato, MQRMcav, MQRMato, MBScav, MBSato])
+    
